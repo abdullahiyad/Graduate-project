3df4a34fa65634aab3d219dd25a3553541e1b26d
Oday Aqel <126450503+OdayAqel@users.noreply.github.com>
2024-03-25 16:02:54 +0200

    Update server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 4000;
+
+app.get('/',(req,res)=>{
+    res.sendFile('../html/login.html',{ root: __dirname });
+})
+
+
+app.listen(PORT,()=>console.log('server connect successfully!!'));
