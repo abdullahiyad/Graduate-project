f3a33fdf96259f57463b505300c06285c15840e2
abdullahiyad <abdullahiyad10@gmail.com>
2024-04-07 16:19:01 +0300

    new update abdullah

--- controller/authController.js
+++ controller/authController.js
@@ -1,49 +1,47 @@
 const user = require("../nodejs/Database/models/users");
-const cookie = require('cookie-parser');
-let errors = {name:'',phone:'',email:'',password:''};
+const cookie = require("cookie-parser");
+let errors = { name: "", phone: "", email: "", password: "" };
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const handleErrors = (err) => {
-    if(err.code === 11000){
-        errors.email = 'AIU';
-        errors.phone = 'AIU';
-        return errors;
-    }
-    if(err.message.includes('user validation failed')){
-        Object.values(err.errors).forEach(({properties}) => {
-            errors [properties.path] = properties.message;
-        })
-    }
+  if (err.code === 11000) {
+    errors.email = "AIU";
+    errors.phone = "AIU";
     return errors;
-} 
-module.exports.signup_get = (req,res) =>{
-    res.render('signup');
-}
-module.exports.login_get = (req,res) =>{
-    res.render('login');
-}
-module.exports.signup_post = async (req,res) =>{
+  }
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+module.exports.signup_get = (req, res) => {
+  res.render("signup");
+};
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
+module.exports.signup_post = async (req, res) => {
   const { name, phone, email, password } = req.body;
   try {
     const users = await user.create({ name, phone, email, password });
     const token = user.createToken(users._id);
-    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
-    res.status(201).json({user: users._id}).redirect('/login');
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: users._id }).redirect("/login");
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).send(errors);
   }
-}
-module.exports.login_post = async (req,res) =>{
-  const {email, password} = req.body;
+};
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const userLogin = await user.login(email,password);
-    res.status(200).json({user: userLogin._id}).redirect('/home');
+    const userLogin = await user.login(email, password);
+    res.status(200).json({ user: userLogin._id }).redirect("/home");
   } catch (err) {
     res.status(400).json({});
   }
-
-
 
   // try {
   //   const check = await user.findOne({email: req.body.email});
@@ -59,17 +57,17 @@ module.exports.login_post = async (req,res) =>{
   //   }
   // } catch {
   //   console.log('wrong details');
-  // } 
-}
-module.exports.home_get = (req,res) =>{
-    res.render('home');
-}
-module.exports.home_post = (req,res) =>{
-    res.send('This is home page');
-}
-module.exports.menu_get = (req,res) =>{
-    res.render('menu');
-}
-module.exports.menu_post = (req,res) =>{
-    res.send('This is menu page');
-}
+  // }
+};
+module.exports.home_get = (req, res) => {
+  res.render("home");
+};
+module.exports.home_post = (req, res) => {
+  res.send("This is home page");
+};
+module.exports.menu_get = (req, res) => {
+  res.render("menu");
+};
+module.exports.menu_post = (req, res) => {
+  res.send("This is menu page");
+};
--- js/login.js
+++ js/login.js
@@ -2,44 +2,40 @@ let loading = document.querySelector(".loader");
 window.addEventListener("load", function () {
   loading.style.display = "none";
 });
-let loginBtn = document.querySelector(".login-btn");
-let username = document.querySelector(".email");
-let pass = document.querySelector(".password");
-loginBtn.addEventListener("click", () => {
-  window.alert(username.textContent, pass.textContent);
-});
-
 
 //This code written by Oday
-const form = document.querySelector('form');
-const emailError = document.querySelector('.email.error');
-const passwordError = document.querySelector('.password.error');
+const form = document.querySelector("form");
+const error = document.querySelector(".error");
 
+//show error message
+function errorMsg() {
+  error.style.display = "block";
+}
+module.exports = { errorMsg };
 
-form.addEventListener('submit',async (e) => {
-  e.preventDefault();
+// form.addEventListener("submit", async (e) => {
+//   e.preventDefault();
 
-  // emailError.textContent = '';
-  // passwordError.textContent = '';
-  const email = form.email.value;
-  const password = form.password.value;
+//   // emailError.textContent = '';
+//   // passwordError.textContent = '';
+//   const email = form.email.value;
+//   const password = form.password.value;
 
-  try {
-    const res = await fetch('/login',{
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: {'Content-Type':'application/json'}
-    });
-    const data = await res.json();
-    console.log(data);
-    if(data.error){
-      emailError.textContent = data.errors.email;
-      password.textContent = data.errors.password;
-    }
-    // if(data.user){
-    //   location.assign('/home');
-    // }
-  } catch (err) {
-    console.log(err);
-  }
-});
+//   try {
+//     const res = await fetch("/login", {
+//       method: "POST",
+//       body: JSON.stringify({ email, password }),
+//       headers: { "Content-Type": "application/json" },
+//     });
+//     const data = await res.json();
+//     console.log(data);
+//     if (data.error) {
+//       error.style.display = "block";
+//     }
+//     // if(data.user){
+//     //   location.assign('/home');
+//     // }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
