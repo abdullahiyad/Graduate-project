5775ea5ad773c8756a95dc87677bddc7fc69eb2c
abdullahiyad <abdullahiyad10@gmail.com>
2024-05-13 14:40:32 +0300

    update messages page and finish it

--- js/admin-messages.js
+++ js/admin-messages.js
@@ -1,6 +1,6 @@
 //to show the links list in medium and small screens
 let listIcon = document.querySelector("#menu-icon");
-let dashboard = document.querySelector(".product-container");
+let dashboard = document.querySelector(".messages-container");
 listIcon.addEventListener("click", () => {
   dashboard.classList.toggle("activeList");
 });
@@ -10,7 +10,120 @@ function readMessagge(event) {
   const clickedElement = event.target;
   clickedElement.parentElement.classList.toggle("more-details");
 }
-document.previousElementSibling;
+function closeMessagge(event) {
+  const clickedElement = event.target;
+  clickedElement.parentElement.parentElement.classList.toggle("more-details");
+}
+let reservationID;
+function rejectReservation(event) {
+  const clickedElement = event.target;
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, Reject it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Rejected!",
+        text: "the reservation request has been rejected.",
+        icon: "success",
+      });
+      reservationID =
+        clickedElement.parentElement.previousElementSibling
+          .previousElementSibling.previousElementSibling.lastElementChild
+          .lastElementChild.value;
+      //function to send this reservation info to rejected reservation database
+      //
+      //function here
+      //
+      clickedElement.parentElement.parentElement.remove();
+    }
+  });
+}
+let messagesTable = document.querySelector(".messages-table");
+function addMessage(
+  userName,
+  userEmail,
+  reservationId,
+  reservationName,
+  reservationPhone,
+  reservationDate,
+  reservationTime,
+  numOfPersons,
+  moreDetails = "none"
+) {
+  const newMessage = document.createElement("div");
+  newMessage.innerHTML = `
+   <form action="" class="message flex-row" method="">
+                    <div class="info-container flex-row">
+                        <div class="icon flex-row">
+                            <i class="fa-regular fa-envelope"></i>
+                            <p class="input-style .new-message">new message</p>
+                        </div>
+
+                        <div>
+                            <input type="text" class="customer-name input-style" value="${userName}">
+                            <input type="email" class="customers-email input-style display-none" value="${userEmail}">
+                            <!-- <input type="text" class="customer-info input-style display-none" value="0569912325"> -->
+                            <input type="text" class="reservation-id" value="${reservationId}">
+                        </div>
+                    </div>
+                    <div class="message-content display-none">
+                        <textarea name="" id="" class="message-content input-style">
+
+subject: reservation request
+
+I want to make a reservation at your esteemed cafe.                          
+Please find the reservation details below:                           
+Contact Name: [${reservationName}]
+Contact Phone: [${reservationPhone}]
+Date: [${reservationDate}]
+Time: [${reservationTime}]
+Number of persons: [${numOfPersons}]
+
+We would appreciate it if you could confirm our reservation at your earliest convenience. 
+here more details : [${moreDetails}]
+
+                        </textarea>
+                    </div>
+                    <p class="open-msg" onclick="readMessagge(event)">read</p>
+                    <div class="btns display-none ">
+                        <input type="submit" class="accept-btn btn-style" value="accept">
+                        <input type="button" class="reject-btn btn-style" onclick="rejectReservation(event)"
+                            value="reject">
+                        <input type="button" class="close-msg btn-style" value="close" onclick="closeMessagge(event)">
+                    </div>
+                </form>
+  `;
+  messagesTable.appendChild(newMessage.firstElementChild);
+}
+
+//examples of how to call function
+addMessage(
+  "sati Hamad",
+  "sati@ykpo.com",
+  1,
+  "sati",
+  "0568453235",
+  "25-6-2024",
+  "12:30",
+  5
+);
+addMessage(
+  "abdullah iyad",
+  "abdullah@ykpo.com",
+  1,
+  "ali",
+  "0568453235",
+  "25-6-2024",
+  "12:30",
+  5
+);
+
 // //function to add product
 // let productTable = document.querySelector(".products-table");
 // function addProduct(
