bebd4edd6f185a14f65291b49b65fc346f939a64
abdullahiyad <abdullahiyad10@gmail.com>
2024-05-14 12:31:23 +0300

    update

--- js/login.js
+++ js/login.js
@@ -6,31 +6,34 @@ window.addEventListener("load", function () {
 //This code written by Oday
 const form = document.querySelector("form");
 const error = document.querySelector(".error");
-const userStatus = 
-//Function to get and dealing with localStorage
+const userStatus =
+  //Function to get and dealing with localStorage
 
-window.addToLocalStorage = function() {
-  fetch('/login', {
-    method: 'POST' // Change the method to POST
-  }).then(async(response) => {
-    if(!response.ok){
-      throw new Error('Network response was not ok');
-    }
-    let data = await response.json();
-    return data;
-  }).then(data => {
-    data.users(user =>{
-      addStatus(user.status);
-    });
-  }).catch((err) => {
-    console.log(err);
+  (window.addToLocalStorage = function () {
+    fetch("/login", {
+      method: "POST", // Change the method to POST
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          errorMsg();
+          throw new Error("Network response was not ok");
+        }
+        let data = await response.json();
+        return data;
+      })
+      .then((data) => {
+        data.users((user) => {
+          addStatus(user.status);
+        });
+      })
+      .catch((err) => {
+        errorMsg();
+      });
   });
-}
 function addStatus(userStatus) {
-  window.localStorage.setItem('status', userStatus);
+  window.localStorage.setItem("status", userStatus);
 }
 //show error message
 function errorMsg() {
   error.style.display = "block";
 }
-module.exports(errorMsg);
