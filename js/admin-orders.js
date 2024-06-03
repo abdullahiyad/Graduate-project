5feab42c61a89830fd8375e9cf05e5dd0d8391ef
abdullahiyad <abdullahiyad10@gmail.com>
2024-06-03 20:36:48 +0300

    finishing the admin orders page

--- js/admin-orders.js
+++ js/admin-orders.js
@@ -15,139 +15,106 @@ function closeMessagge(event) {
   clickedElement.parentElement.parentElement.classList.toggle("more-details");
 }
 
-// if reject reservation
-//let reservationID;
-function rejectReservation(event) {
-  const clickedElement = event.target;
-  Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, Reject it!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: "Rejected!",
-        text: "The reservation request has been rejected.",
-        icon: "success",
-      });
-
-      // Extract reservation ID from the DOM
-      const reservationID =
-        clickedElement.parentElement.previousElementSibling
-          .previousElementSibling.previousElementSibling.lastElementChild
-          .lastElementChild.value;
-      console.log(reservationID);
-      // Send the reservation ID and state to the server
-      fetch("/admin/messages", {
-        method: "PUT", // Specify the HTTP method as PUT
-        headers: {
-          "Content-Type": "application/json", // Specify the content type
-        },
-        body: JSON.stringify({ id: reservationID, state: "rej" }), // Send the reservation ID and state
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("There is something wrong");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log("Reservation update successful:", data);
-          // Handle the success response here
-          clickedElement.parentElement.parentElement.remove();
-        })
-        .catch((err) => {
-          console.error("Reservation update failed:", err);
-        });
-    }
-  });
-}
-let messagesTable = document.querySelector(".messages-table");
-function addMessage(
+let ordersTable = document.querySelector(".orders-table");
+function addOrder(
   userName,
   userEmail,
-  reservationId,
-  reservationName,
-  reservationPhone,
-  reservationDate,
-  numOfPersons,
-  moreDetails = "none"
+  orderName,
+  orderPhone,
+  city,
+  address1,
+  address2,
+  prodctsArray,
+  totalPrice
 ) {
-  const newMessage = document.createElement("div");
-  newMessage.innerHTML = `
-   <form action="" class="message flex-row" method="">
+  const products = document.createElement("tbody");
+  prodctsArray.forEach((element) => {
+    products.appendChild(addProduct(element.name, element.quan));
+  });
+  const newOrder = document.createElement("div");
+  newOrder.innerHTML = `
+   <div class="message flex-row">
                     <div class="info-container flex-row">
                         <div class="icon flex-row">
-                            <i class="fa-regular fa-envelope"></i>
-                            <p class="input-style .new-message">new message</p>
+                            <i class="fa-solid fa-utensils"></i>
+                            <p class="input-style .new-message">new order</p>
                         </div>
 
-                        <div>
+                        <div class="user-details">
                             <input type="text" class="customer-name input-style" value="${userName}">
                             <input type="email" class="customers-email input-style display-none" value="${userEmail}">
                             <!-- <input type="text" class="customer-info input-style display-none" value="0569912325"> -->
-                            <input type="text" class="reservation-id" value="${reservationId}">
                         </div>
                     </div>
                     <div class="order-content display-none">
-                        <textarea name="" id="" class="order-content input-style">
-
-subject: reservation request
-
-I want to make a reservation at your esteemed cafe.                          
-Please find the reservation details below:                           
-Contact Name: [${reservationName}]
-Contact Phone: [${reservationPhone}]
-Date: [${reservationDate}]
-Number of persons: [${numOfPersons}]
-
-We would appreciate it if you could confirm our reservation at your earliest convenience. 
-here more details : [${moreDetails}]
-
-                        </textarea>
+                        <div class="order-content input-style">
+                            <div class=" display-none order-info ">
+                                <h3>order information :</h3>
+                                <div class="name input-style">name : ${orderName}</div>
+                                <div class="phone input-style">phone : ${orderPhone}</div>
+                                <div class="city input-style">city : ${city}</div>
+                                <div class="address1 input-style">address : ${address1}</div>
+                                <div class="address2 input-style">address2 : ${address2}</div>
+                            </div>
+                            <div class="order-details">
+                                <h3>order details : </h3>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>product</th>
+                                            <th>quantity</th>
+                                        </tr>
+                                    </thead>
+                                    ${products.innerHTML}
+                                    <tfoot>
+                                        <td>total price :${totalPrice} $</td>
+                                        <td></td>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                    <p class="open-order" onclick="readMessagge(event)">read</p>
+                    <p class="open-order" onclick="readMessagge(event)">details</p>
                     <div class="btns display-none ">
-                        <button class="accept-btn btn-style" onclick="acceptReservation(event)">Accept</button>
-                        <input type="button" class="reject-btn btn-style" onclick="rejectReservation(event)"
-                            value="reject">
                         <input type="button" class="close-msg btn-style" value="close" onclick="closeMessagge(event)">
                     </div>
-                </form>
+                </div>
   `;
-  messagesTable.appendChild(newMessage.firstElementChild);
+  ordersTable.appendChild(newOrder.firstElementChild);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("/admin/messages/api")
-    .then(async (response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      let data = await response.json();
-      console.log(data);
-      return data;
-    })
-    .then((data) => {
-      data.forEach((reservation) => {
-        addMessage(
-          reservation.customer.userName,
-          reservation.customer.userEmail,
-          reservation._id,
-          reservation.resName,
-          reservation.phone,
-          new Date(reservation.newDate).toLocaleString(), // Format date
-          reservation.numPerson,
-          reservation.details
-        );
-      });
-    })
-    .catch((error) => console.error("Error fetching user data:", error));
-});
+///
+
+//test the function
+const productsArr = [
+  { name: "coffee", quan: 3 },
+  { name: "tea", quan: 2 },
+];
+addOrder(
+  "ahmed",
+  "abdullahiyad@gmail.com",
+  "ali",
+  "1571681681",
+  "jenin",
+  "arabah",
+  "address2",
+  productsArr,
+  300
+);
+
+///
+
+//function add products
+function addProduct(productName, productQuantity) {
+  const row = document.createElement("tr");
+  const col1 = document.createElement("td");
+  col1.textContent = productName;
+  const col2 = document.createElement("td");
+  col2.textContent = productQuantity;
+  row.appendChild(col1);
+  row.appendChild(col2);
+  return row;
+}
 
 window.logout = function () {
   fetch("/admin/messages/logout", {
@@ -160,45 +127,3 @@ window.logout = function () {
       console.log(err);
     });
 };
-
-// if accept reservation
-function acceptReservation(event) {
-  event.preventDefault(); // Prevent default button behavior
-  const clickedElement = event.target;
-  const reservationID =
-    clickedElement.parentElement.previousElementSibling.previousElementSibling
-      .previousElementSibling.lastElementChild.lastElementChild.value;
-  console.log(reservationID);
-  fetch("/admin/messages", {
-    method: "PUT", // Specify the HTTP method as PUT for updating
-    headers: {
-      "Content-Type": "application/json", // Specify the content type
-    },
-    body: JSON.stringify({ id: reservationID, state: "acc" }), // Send the reservation ID and state
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("There is something error");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Reservation accepted successfully:", data);
-      Swal.fire({
-        title: "Accepted!",
-        text: "The reservation request has been accepted.",
-        icon: "success",
-      });
-      // Optionally, you can update the UI to reflect the change
-      // For example, remove the accepted reservation from the DOM
-      clickedElement.parentElement.parentElement.remove();
-    })
-    .catch((err) => {
-      console.error("Reservation acceptance failed:", err);
-      Swal.fire({
-        title: "Error!",
-        text: "There was an error accepting the reservation.",
-        icon: "error",
-      });
-    });
-}
